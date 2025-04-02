960d2fca478fac8e7a4a4167af2f168f811ff710
Nischal Regmi <rnischal593@gmail.com>
2025-04-02 13:13:55 +0545

    updated_again

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
-from home.views import addmissionapi
+from home.views import addmissionapi, admissionshowapi
 from django.urls import path
 
 urlpatterns = [
     path('admission_registration/', addmissionapi.as_view()),
+    path('admissiondata_show/', admissionshowapi.as_view()),
 ]
--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from .serializer import StudentSerializer
+from .models import Admission_data
 # Create your views here.
 
 class addmissionapi(APIView):
@@ -14,3 +15,9 @@ class addmissionapi(APIView):
             return Response(student.data)
         else:
             return Response(student.errors)
+
+class admissionshowapi(APIView):
+    def get(self, request):
+        students=Admission_data.objects.all()
+        object=StudentSerializer(students, many=True)
+        return Response(object.data)
